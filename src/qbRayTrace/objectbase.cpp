153ec468696a329f51ecefabec54ebe1efc38d69
Kardo Marof (consultant) <kardo.marof@consultant.volvo.com>
2024-03-17 17:30:33 +0100

    Minor adjustments to object classes

--- src/qbRayTrace/objectbase.cpp
+++ src/qbRayTrace/objectbase.cpp
@@ -2,4 +2,22 @@
 #include <math.h>
 #define EPSILON 1e-21f;
 
-qbRT::ObjectBase::ObjectBase(){}
+qbRT::ObjectBase::ObjectBase(){
+
+}
+
+qbRT::ObjectBase::~ObjectBase()
+{
+
+}
+
+bool qbRT::ObjectBase::TestIntersection(const Ray &castRay, qbVector<double> &intPoint, qbVector<double> &localNormal, qbVector<double> &localColor)
+{
+    return false;
+}
+
+bool qbRT::ObjectBase::CloseEnough(const double f1, const double f2)
+{
+    return fabs(f1-f2) < EPSILON;
+}
+
---
+++ src/qbRayTrace/objectsphere.hpp
@@ -0,0 +1,21 @@
+#ifndef OBJECTSPHERE_H
+#define OBJECTSPHERE_H
+
+#include "objectbase.hpp"
+
+namespace qbRT
+{
+    class ObjSphere : public ObjectBase
+    {
+        public:
+            ObjSphere()
+
+            virtual ~ObjSphere() override;
+
+            virtual bool TestIntersection(const Ray &castRay, qbVector<double> &intPoint, qbVector<double> &localNormal, qbVector<double> &localColor);
+
+        private:
+        
+
+    };
+}
